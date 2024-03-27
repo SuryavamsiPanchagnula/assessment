2d711861201b3f6fb186d9777bc097c260264838
Admin <psuryavamsi88@gmail.com>
2024-03-28 00:47:13 +0530

    second commit

---
+++ src/assessment/assignment2.java
@@ -0,0 +1,25 @@
+package assessment;
+
+import java.util.Scanner;
+
+public class assignment2 {
+	
+	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter first number");
+		
+		int a = sc.nextInt();
+		
+		System.out.println("Enter second number");
+		
+		int b = sc.nextInt();
+		
+		int c=a+b;
+		
+		System.out.print(c);
+	}
+
+}
